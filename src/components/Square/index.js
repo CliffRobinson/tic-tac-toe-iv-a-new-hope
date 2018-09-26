4adcfa69527da258a71a9b4fcb3b3ec27fb71150
CliffRobinson <Clifford.f.robinson@gmail.com>
2018-09-26 16:56:15 +1200

    board colors, you're not my real dad charles

--- src/components/Square/index.js
+++ src/components/Square/index.js
@@ -2,15 +2,16 @@ import React from 'react'
 import styled from 'styled-components'
 
 const StyledSquare = styled.div`
-    border-color: hsla(0, 0%, 0%, 0.2);
-    border-style: solid:
-    border-width: 2px;
-    color:gray;
-    font-size: 16vh;
-    font-weight:bold;
-    line-height: 20vh;
-    text-align:center;
-    text-transform: uppercase;
+  border-color: hsla(0, 0%, 0%, 0.2);
+  border-style: solid;
+  border-width: 0 ${props => (props.index % 3 === 2 ? 0 : '2px')}
+    ${props => (props.index < 6 ? '2px' : 0)} 0;
+  color: ${props => (props.player === 'x' ? 'blue' : 'gold')};
+  font-size: 16vh;
+  font-weight: bold;
+  line-height: 20vh;
+  text-align: center;
+  text-transform: uppercase;
 `
 
 StyledSquare.displayName = 'StyledSquare'
