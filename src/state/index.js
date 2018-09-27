6fed422e351bbee634e5c5d5833bedc94e44543f
CliffRobinson <Clifford.F.Robinson@gmail.com>
2018-09-28 11:47:41 +1200

    store?

--- src/state/index.js
+++ src/state/index.js
@@ -4,6 +4,13 @@ import { SQUARE_CLICKED } from './constants'
 import { squareClicked } from './actions'
 import { initialState, rootReducer } from './reducers'
 import { getMoves } from './selectors'
+import configureStore from './store'
 
-export { getMoves, initialState, rootReducer, SQUARE_CLICKED, squareClicked }
-// test tiems!
+export {
+  configureStore,
+  getMoves,
+  initialState,
+  rootReducer,
+  SQUARE_CLICKED,
+  squareClicked
+}
