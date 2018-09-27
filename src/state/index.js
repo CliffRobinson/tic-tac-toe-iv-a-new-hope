19bdc7740be4046488780c54e0b732608b0e2b0c
CliffRobinson <Clifford.F.Robinson@gmail.com>
2018-09-28 11:38:28 +1200

    selectors?!?!?!

--- src/state/index.js
+++ src/state/index.js
@@ -3,5 +3,7 @@
 import { SQUARE_CLICKED } from './constants'
 import { squareClicked } from './actions'
 import { initialState, rootReducer } from './reducers'
+import { getMoves } from './selectors'
 
-export { SQUARE_CLICKED, squareClicked }
+export { getMoves, initialState, rootReducer, SQUARE_CLICKED, squareClicked }
+// test tiems!
