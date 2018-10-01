2732e8fe69fbc64e175c2f3ebfcbe090d14cb9a3
CliffRobinson <Clifford.F.Robinson@gmail.com>
2018-10-01 17:16:49 +1300

    update reducer for game_over action

--- src/state/reducers/index.js
+++ src/state/reducers/index.js
@@ -1,20 +1,26 @@
 // src/state/reducers/index.js
 import { isUndefined } from 'ramda-adjunct'
 
-import { SQUARE_CLICKED } from '..'
+import { SQUARE_CLICKED, GAME_OVER } from '..'
 
 const initialState = {
   moves: []
 }
 
 function rootReducer (state = initialState, { payload = {}, type }) {
+  const { square, winners: { squares, player } = {} } = payload
+
   switch (type) {
+    case GAME_OVER:
+      return {
+        ...state,
+        winningSquares: squares,
+        winningPlayer: player
+      }
     case SQUARE_CLICKED:
       return {
         ...state,
-        moves: isUndefined(payload.square)
-          ? state.moves
-          : [...state.moves, payload.square]
+        moves: isUndefined(square) ? state.moves : [...state.moves, square]
       }
     default:
       return state
